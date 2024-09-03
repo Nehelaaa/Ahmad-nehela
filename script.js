ae2b941de7c0969266f70d8132f0db72d6908525
Ahmad_Nehela <60977296+Nehelaaa@users.noreply.github.com>
2024-09-03 13:12:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-Skip to content
-Commands
-Search
-Recent
-Cloud Services
-Workspace Features
-Collaboration
-Other
-Workspace Extensions
-None installed.
-
-
-
-
-
-/
-
-
-
-
-
-
-
-
-
-
-Selection deleted
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
@@ -32,31 +5,63 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
-        if(this.scrollY > 500){
+        if(this.scroll > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
         }
     });
+   
 
     // slide-up script
+
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
     });
 
-    //toggle menu/navbar script
+    /*toggle menu/navbar script */
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing animation script
+
+    //yping animation script 
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Innovator", "Freelancer", "Web Developer", "Entrepreneur"],
+        strings: ["Developer", "Designer", "Innovator", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-- Replit
+        strings: ["Developer", "Designer", "Innovator","Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+
+    });
+});   
